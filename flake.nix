3cd61b4b519d0eb4b902df78604ac7fb23e9eda8
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2024-04-20 13:34:15 -0400

    baseline init

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    {
+      nixosConfigurations.test = nixpkgs.lib.nixosSystem {
+        modules = [
+          (
+            { modulesPath, ... }:
+            {
+              imports = [ "${modulesPath}/profiles/minimal.nix" ];
+
+              boot.loader.grub.enable = false;
+              fileSystems."/".device = "nodev";
+              nixpkgs.hostPlatform = "x86_64-linux";
+              system.stateVersion = "24.05";
+
+              # custom config here
+              programs.nh = {
+                enable = true;
+                flake = null;
+              };
+            }
+          )
+        ];
+      };
+    };
+}
